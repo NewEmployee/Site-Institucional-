669185134b0dba855d88e80886e95cc524140b0c
Diogo Henrique Gomes <diogo.gomes@sptech.school>
2022-05-26 15:36:45 -0300

    Dashboard dinamica

--- src/models/medidaModel.js
+++ src/models/medidaModel.js
@@ -1,25 +1,24 @@
 var database = require("../database/config");
 
-function buscarUltimasMedidas(idAquario, limite_linhas) {
+function buscarUltimasMedidas(idSensor, limite_linhas) {
     instrucaoSql = `select 
-                        temperatura, 
-                        umidade, 
-                        momento,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico
-                    from medida
-                    where fk_aquario = ${idAquario}
-                    order by id desc limit ${limite_linhas}`;
+                        valor_registro, 
+                        data_hora,
+                        DATE_FORMAT(data_hora,'%H:%i:%s') as momento_grafico
+                        from registro_sensor
+                        where fk_sensor = ${idSensor}
+                        order by id_registro desc limit ${limite_linhas}`;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
-function buscarMedidasEmTempoReal(idAquario) {
-    instrucaoSql = `select 
-                        temperatura, 
-                        umidade, DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
-                        fk_aquario 
-                        from medida where fk_aquario = ${idAquario} 
-                    order by id desc limit 1`;
+function buscarMedidasEmTempoReal(idSensor) {
+            instrucaoSql = `select
+                            valor_registro, 
+                            data_hora,
+                            DATE_FORMAT(data_hora,'%H:%i:%s') as momento_grafico
+                            from registro_sensor where fk_sensor = ${idSensor} 
+                            order by id_registro desc limit 1`;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
