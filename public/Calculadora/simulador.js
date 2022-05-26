5be87251f5d1d0cbb85af031e6805cbac33d231a
Diogo <diogo.gomes@sptech.school>
2022-05-26 19:37:37 -0300

    Simulador finalizado

--- public/Calculadora/simulador.js
+++ public/Calculadora/simulador.js
@@ -5,20 +5,17 @@
     var jornada = Number(horas.value)
     var diasTrab = Number(diasTrabalhados.value)
     var tarefa = Number(input_tarefas.value)
-    var tarefa_custo = Number(input_tarefas_custo.value)
-    // var falta = Number(faltas.value)
-    // var atraso = Number(atrasos.value)
-    // var salario = Number(mSalario.value)
+    var tarefa_lucro = Number(input_tarefas_lucro.value)
 
 
-    var produtividade_antes = tarefa / jornada
-    var produtividade_depois = tarefa * 1.35 / jornada
+    var produtividade_antes = tarefa
+    var produtividade_depois = tarefa * 1.35
 
-    var custo_antes = tarefa_custo * (tarefa * qtdFuncionario)
-    var custo_depois = tarefa_custo * ((tarefa * 1.35) * qtdFuncionario)
+    var lucro_antes = tarefa_lucro * tarefa
+    var lucro_depois = tarefa_lucro * (tarefa * 1.35)
 
 
-    if (nome != "" && qtdFuncionario > 0 && jornada > 0 && jornada <= 24 && diasTrab > 0 && diasTrab <= 31 && tarefa > 0 && tarefa_custo > 0 ){
+    if (nome != "" && qtdFuncionario > 0 && jornada > 0 && jornada <= 24 && diasTrab > 0 && diasTrab <= 31 && tarefa > 0 && tarefa_lucro > 0 ){
     
       mensagem_erro.innerHTML = "";
       document.getElementById("h3_nome").style.color = "black";
@@ -26,7 +23,7 @@
       document.getElementById("h3_funcionarios").style.color = "black";
       document.getElementById("h3_dias").style.color = "black";
       document.getElementById("h3_tarefas").style.color = "black";
-      document.getElementById("h3_tarefas_custo").style.color = "black";
+      document.getElementById("h3_tarefas_lucro").style.color = "black";
       
     } else { 
 
@@ -55,10 +52,10 @@
       } else {
         document.getElementById("h3_tarefas").style.color = "black";
       }
-      if (tarefa_custo <= 0){
-        document.getElementById("h3_tarefas_custo").style.color = "red";
+      if (tarefa_lucro <= 0){
+        document.getElementById("h3_tarefas_lucro").style.color = "red";
       } else {
-        document.getElementById("h3_tarefas_custo").style.color = "black";
+        document.getElementById("h3_tarefas_lucro").style.color = "black";
       }
       
       cardErro.style.display = "block"
@@ -67,26 +64,14 @@
     }
     
     var horasMes = qtdFuncionario * jornada * diasTrab
-    // var horasPerdidas = falta * jornada + atraso
-
-    // var indice = horasPerdidas / horasMes * 100
-
-    // var indice_depois = indice * 0.49
-
-    // var salarioHora = salario / diasTrab / jornada
-
-    // var perca_antes = salarioHora * horasPerdidas
-    // var perca_depois = salarioHora * (horasPerdidas  * 0.49)
-
     h2_bemvindo.innerHTML = `Bem vindo, `
     h2_NomeEmpresa.innerHTML = `${nome}`
     p_horas.innerHTML = `${horasMes}h`
-    // p_abstencao.innerHTML = `${horasPerdidas} H`
-    p_valor.innerHTML= `R$ ${tarefa_custo.toFixed(2)}`
-    div_perca_antes.innerHTML = `Ganho mensal: <br>R$ ${custo_antes.toFixed(2)}`
-    div_perca_depois.innerHTML = `Ganho mensal: <br>R$ ${custo_depois.toFixed(2)}`
-    div_abs_antes.innerHTML = `Diária/média: ${produtividade_antes.toFixed(2)} `
-    div_abs_depois.innerHTML = `Diária/média  : ${produtividade_depois.toFixed(2)} `
+    p_valor.innerHTML= `R$ ${tarefa_lucro.toFixed(2)}`
+    div_perca_antes.innerHTML = `Ganho mensal: <br>R$ ${lucro_antes.toFixed(2)}`
+    div_perca_depois.innerHTML = `Ganho mensal: <br>R$ ${lucro_depois.toFixed(2)}`
+    div_abs_antes.innerHTML = `Itens produzidos mensalmente: ${produtividade_antes.toFixed(0)} `
+    div_abs_depois.innerHTML = `Itens produzidos mensalmente: ${produtividade_depois.toFixed(0)} `
     div_info_texto.innerHTML = `<br> Nossos serviços podem aumentar<br>em até 35% da produtividade das squads.`
 
     document.getElementById("informacoes_Princ").style.visibility = 'visible';
@@ -100,7 +85,7 @@
     ];
   
     const data = {
-    labels: ['Itens diários'],
+    labels: ['Produção mensal'],
     datasets: [{
       // axis: 'y',
       label: 'Antes',
@@ -130,8 +115,7 @@
   const config = {
     type: 'bar',
     data,
-    options: {
-    // indexAxis: 'y',  
+    options: { 
   }
   };
 
@@ -155,7 +139,7 @@
     datasets: [{
       axis: 'y',
       label: 'Antes',
-      data: [custo_antes],
+      data: [lucro_antes],
       backgroundColor: [
         'rgba(255, 51, 51, 0.4)'
       ],
@@ -167,7 +151,7 @@
     {
       axis: 'y',
       label: 'Depois',
-      data: [custo_depois],
+      data: [lucro_depois],
       backgroundColor: [
         'rgba(0, 150 ,255, 0.4)'
       ],
